402d0f5d805e5791b90d244dc0d2a292747247ec
Ayoub Akritah <aakritah@c1r3p3.1337.ma>
2025-05-15 19:38:18 +0100

    add

--- main.c
+++ main.c
@@ -6,31 +6,7 @@
 /*   By: aakritah <aakritah@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/25 13:18:28 by aakritah          #+#    #+#             */
-/*   Updated: 2025/04/09 14:42:51 by aakritah         ###   ########.fr       */
+/*   Updated: 2025/05/15 19:38:07 by aakritah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "./include/main.h"
-#include "./include/parse.h"
-
-int	main(int ac, char **av, char **env)
-{
-	char	*str;
-	int		i;
-
-	i = 0;
-	while (i < 1)
-	{
-		str = readline("Minishell > ");
-		if (str && str[0])
-		{
-			ft_parse(str);
-			add_history(str);
-		}
-		else if(!str) 
-			break ;
-		free(str);
-		i++;
-	}
-	return (0);
-}
